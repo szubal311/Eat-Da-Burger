fe719336c5432d83e3b70a0d6a5436d4277a892b
szubal311 <73494903+szubal311@users.noreply.github.com>
2021-03-13 21:13:20 -0700

    created seeds sql file

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers ( burger_name, devoured)
+VALUES
+("Celestial Burger", false),
+("The Big Benny", false),
+("The Bear Burger", false),
+("Haystack Double", false),
+("Garlic Burger", false);
