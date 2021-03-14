a328ab0338954fca100fab50a801575c8d0fc322
szubal311 <73494903+szubal311@users.noreply.github.com>
2021-03-13 20:51:08 -0700

    made additions to controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { end } = require('../config/connection');
 const hbsExp = require
 const router = express.Router();
 
@@ -12,23 +13,52 @@ router.get ('/', (req,res) => {
         console.log(hbsObject);
         res.render('index', hbsObject);
     });
-    
-    router.post('/api/burgers', (req, res) =>{
-        let devoured = 0;
-        if (req.body.devoured === 'true') {
-            devoured = 1;
-        }
-
-        daBurger.create(
-            ['burger_name', 'devoured'],
-            [req.body.burger_name, devoured],
-            (result) => {
-                console.log(result);
-            }
-        );
-    });
-
-    router.put('/api/burgers/id:', (req, res) => {
-        const 
-    })
 });
+
+router.post('/api/burgers', (req, res) =>{
+    let devoured = 0;
+    if (req.body.devoured === 'true') {
+        devoured = 1;
+    }
+
+    daBurger.create(
+        ['burger_name', 'devoured'],
+        [req.body.burger_name, devoured],
+        (result) => {
+            console.log(result);
+        }
+    );
+});
+
+router.put('/api/burgers/id:', (req, res) => {
+    const condition = `id ${req,params.id}`;
+
+    console.log('condition', condition);
+
+    daBurger.update(
+        {
+            devoured: req.body.devoured,
+        },
+        condition,
+        (result) => {
+            if (result.changedRows ==0) {
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        }           
+    );
+});
+
+router.delete('/api/burgers/:id', (req, res) => {
+    const condition = `id ${req,params.id}`;
+
+    daBurger.delete(condition, (result) => {
+        if (result.afftectedRows == 0) {
+            return res.status(404).end();
+        }
+        res.status(200).end();
+    });
+});
+
+module.exports = router;
+
