67caa20638a970b7e4f07ed2637b262189896c33
szubal311 <73494903+szubal311@users.noreply.github.com>
2021-03-13 21:08:41 -0700

    created sql shema file

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE  IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR (50),
+    devoured BOOLEAN,
+    PRIMARY KEY (id)
+);
