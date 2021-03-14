7ffb9252e34e837cbe96db80785aafdfbbdde230
szubal311 <73494903+szubal311@users.noreply.github.com>
2021-03-13 21:27:43 -0700

    added code to the burgers.js file

---
+++ models/burgers.js
@@ -0,0 +1,29 @@
+const orm = require('../config/orm');
+
+const yumBurger = {
+    all(cb){
+        orm.all('burgers', (res) => {
+            cb(res);
+        });
+    },
+
+    create(cols, vals, cb) {
+        orm.create('burgers', cols, vals, (res) => {
+            cb(res);
+        });
+    },
+
+    update(objColVals, condition, cb) {
+        orm.update('burgers', objColVals, condition, (res) => {
+            cb(res);
+        });
+    },
+
+    delete(condition, cb) {
+        orm.delete('burgers', condition, (res) => {
+            cb(res);
+        });
+    },
+};
+
+module.exports = yumBurger
