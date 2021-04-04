6ae9ab304f1ead3c65179d8f7765413d43605c8b
szubal311 <73494903+szubal311@users.noreply.github.com>
2021-04-04 12:50:36 -0600

    created a js file to handle the click functions

---
+++ public/js/script.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    if (event) {
+        console.info('DOM loaded');
+    }
+
+    const changeDevour = document.querySelectorAll('.changeDevour');
+
+    if (changeDevour) {
+        changeDevour.forEach((button) => {
+            button.addEventListener('click', (e) => {
+                const id = e.target.getAttribute('data-id');
+                console.log(`I am id ${id}`)
+
+                fetch(`/api/burgers/${id}`, {
+                    method: 'PUT',
+                    hearders: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+                }).then((response) => {
+                    if (response.ok) {
+                        console.log(`devoured the burger`);
+                        locationreload('/');
+                    } else {
+                        alert('an error has occured');
+                    }
+                });
+            });
+        });
+        
+    }
+
+    const burgerBtnCreate = document.getElementById('create-form');
+
+    if (burgerBtnCreate) {
+        burgerBtnCreate.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const burgerNew = {
+                name: document.getElementById('hambur').Value.trim(),
+            };
+            console.log(burgerNew)
+
+            fetch('/api/burgers', {
+                method: 'POST',
+                headers: {
+                    Accept: 'appication/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(burgerNew),
+            }).then(() => {
+                document.getElementById('hambr').value = '';
+                console.log('New burger created');
+                location.reload();
+            });
+        });
+    }
+});
