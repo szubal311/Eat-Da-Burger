376bdcf23d52639ef33c831ccb9cdf14168f3241
szubal311 <73494903+szubal311@users.noreply.github.com>
2021-04-04 10:48:44 -0600

    made a few updates to clean up the code

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const { end } = require('../config/connection');
+const app = express();
 const hbsExp = require('express-handlebars')
 const router = express.Router();
-
 const daBurger = require('../models/burgers');
 
+app.engine('handlebars', hbsExp({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 router.get ('/', (req,res) => {
-    daBurger.all((data) => {
+    daBurger.selectAll((data) => {
         const hbsObject = {
             bugers: data,
         };
@@ -16,30 +18,18 @@ router.get ('/', (req,res) => {
 });
 
 router.post('/api/burgers', (req, res) =>{
-    let devoured = 0;
-    if (req.body.devoured === 'true') {
-        devoured = 1;
-    }
+    daBurger.insertOne([req.body.name], (result) => {
+        res.json(result)
+    })
 
-    daBurger.create(
-        ['burger_name', 'devoured'],
-        [req.body.burger_name, devoured],
-        (result) => {
-            console.log(result);
-        }
-    );
+    
 });
 
 router.put('/api/burgers/id:', (req, res) => {
-    const condition = `id ${req,params.id}`;
+    const id = `${req,params.id}`;
 
-    console.log('condition', condition);
-
-    daBurger.update(
-        {
-            devoured: req.body.devoured,
-        },
-        condition,
+    daBurger.updateOne(
+        id,
         (result) => {
             if (result.changedRows ==0) {
                 return res.status(404).end();
@@ -47,17 +37,6 @@ router.put('/api/burgers/id:', (req, res) => {
             res.status(200).end();
         }           
     );
-});
-
-router.delete('/api/burgers/:id', (req, res) => {
-    const condition = `id ${req,params.id}`;
-
-    daBurger.delete(condition, (result) => {
-        if (result.afftectedRows == 0) {
-            return res.status(404).end();
-        }
-        res.status(200).end();
-    });
 });
 
 module.exports = router;
