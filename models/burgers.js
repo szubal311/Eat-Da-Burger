057e86ee97ea0cc844496d9f347fe7cd2bb0711e
szubal311 <73494903+szubal311@users.noreply.github.com>
2021-04-04 10:58:20 -0600

    made aew tweaks to clean up the code.

--- models/burgers.js
+++ models/burgers.js
@@ -2,28 +2,20 @@ const orm = require('../config/orm');
 
 const yumBurger = {
     all(cb){
-        orm.all('burgers', (res) => {
+        orm.selectAll('burgers', (res) => {
             cb(res);
         });
     },
 
-    create(cols, vals, cb) {
-        orm.create('burgers', cols, vals, (res) => {
-            cb(res);
+    insertOne(reqBody, cb) {
+        orm.insertOne('burgers', reqBody, (res) => {
+            cb(res)
         });
     },
 
-    update(objColVals, condition, cb) {
-        orm.update('burgers', objColVals, condition, (res) => {
-            cb(res);
-        });
-    },
-
-    delete(condition, cb) {
-        orm.delete('burgers', condition, (res) => {
-            cb(res);
-        });
-    },
+    updateOne(id, cb) {
+        orm.updateOne(id, (res) => cb(res));
+    }
 };
 
 module.exports = yumBurger
