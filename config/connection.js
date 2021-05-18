b3b6f65f5d8d68fbfca8aafa37d21ec244d008aa
szubal311 <73494903+szubal311@users.noreply.github.com>
2021-05-17 22:04:08 -0600

    updated connection string

--- config/connection.js
+++ config/connection.js
@@ -3,22 +3,9 @@ const mysql = require('mysql');
 
 let connection;
 
-// if(process.env.PASSWORD){
-//   connection = mysql.createConnnection(process.env.PASSWORD);
-// } else {
-
-// const connection = mysql.createConnection({
-  
-//     host: 'localhost',  
-//     port: 3306,
-//     user: 'root',      
-//     password: process.env.PASSWORD,
-//     database: 'burgers_db',
-//     insecureAuth : true
- 
-  
-// });
-// };
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnnection(process.env.JAWSDB_URL);
+} else {
 
 connection = mysql.createConnection({
   
@@ -29,14 +16,15 @@ connection = mysql.createConnection({
   database: 'burgers_db',
   insecureAuth : true
 });
+};
 // Make connection.
-connection.connect((err) => {
-  if (err) {
-    console.error(`error connecting: ${err.stack}`);
-    return;
-  }
-  console.log(`connected as id ${connection.threadId}`);
-});
-
+// connection.connect((err) => {
+//   if (err) {
+//     console.error(`error connecting: ${err.stack}`);
+//     return;
+//   }
+//   console.log(`connected as id ${connection.threadId}`);
+// });
+connection.connect()
 // Export connection for our ORM to use.
 module.exports = connection;
