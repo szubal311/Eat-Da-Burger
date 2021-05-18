2afc103107cc1cf6f908918cf6b17c04cad7ba3a
szubal311 <73494903+szubal311@users.noreply.github.com>
2021-05-17 22:19:29 -0600

    corrected some spelling

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 let connection;
 
 if(process.env.JAWSDB_URL){
-  connection = mysql.createConnnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 
 connection = mysql.createConnection({
